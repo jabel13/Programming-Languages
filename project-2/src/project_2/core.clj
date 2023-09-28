b1248392f3ea65df5c48a03514803258d996edba
jabel13 <jalcanta@oswego.edu>
2023-09-28 16:43:08 -0400

    Update core.clj

--- project-2/src/project_2/core.clj
+++ project-2/src/project_2/core.clj
@@ -5,3 +5,22 @@
   "I don't do a whole lot ... yet."
   [& args]
   (println "Hello, World!"))
+
+
+(defn not-elimination [not-prop]
+  (println (not not-prop)))
+
+
+
+;; (defn and-elimination [and-prop] ...)
+
+;; (defn modus-ponens [if-prop kb] ...)
+
+;; (defn modus-tollens [if-prop kb] ...)
+
+;; (defn elim-step
+;;   "One step of the elimination inference procedure."
+;;   [prop kb]
+;;   ...)
+
+;; (defn fwd-infer [prop kb] ...)
