ddc18c356e0b6fd9779a3865af6e98b9ef114e86
jabel13 <jalcanta@oswego.edu>
2023-09-30 00:02:00 -0400

    Update core.clj

--- project-2/src/project_2/core.clj
+++ project-2/src/project_2/core.clj
@@ -1,14 +1,28 @@
 (ns project-2.core
   (:gen-class))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
-
-
 (defn not-elimination [not-prop]
-  (println (not not-prop)))
+  ; Check if it's a list
+  (if (list? not-prop)
+    ; Check if it starts with 'not'
+    (if (= 'not (first not-prop))
+      ; Check if the second element is a list starting with 'not'
+      (if (list? (second not-prop))
+        ; If all conditions are met, return a singleton set
+        #{(second (second not-prop))}
+        ; If the second element is not a valid nested 'not', return an empty set
+        #{})
+      ; If it doesn't start with 'not', return an empty set
+      #{})
+    ; If it's not a list, return an empty set
+    #{}))
+
+(defn -main
+  [& args]
+  (println (not-elimination '(not x)))
+  (println (not-elimination '(not (not a))))
+  (println (not-elimination '(not (not (and a b)))))
+  (println (not-elimination '(not (not (not (not c)))))))
 
 
 
