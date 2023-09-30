90bf3ef6c33f4cec087f1e202106f0b46c70a4b7
jabel13 <jalcanta@oswego.edu>
2023-09-30 00:25:44 -0400

    clean up

---
+++ Clojure/micro-project/test/micro_project/core_test.clj
@@ -0,0 +1,7 @@
+(ns micro-project.core-test
+  (:require [clojure.test :refer :all]
+            [micro-project.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
